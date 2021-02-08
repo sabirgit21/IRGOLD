d934ed53c4ad1eeec35bf098637f1c8b815ada6a
ReportServer <ReportServer@192.168.2.4>
2021-02-08 16:18:07 +0500

    Set Payable Supplier setup report,BankCash Reports

--- ViewController/src/view/Report/VoucherPrint.java
+++ ViewController/src/view/Report/VoucherPrint.java
@@ -539,5 +539,53 @@ public class VoucherPrint {
         System.out.println("Url => " + url);
         reportBean.openUrlInNewWindow(url);
     }
-    
+
+    public void getSupplReport(ActionEvent actionEvent) {
+        // Add event code here...
+        String url = "";
+        BigDecimal sendSUPPID = (BigDecimal) actionEvent.getComponent().getAttributes().get("sendSUPPID");
+        reportBean.setReportParameter("P_PBL_SUPP_id", sendSUPPID.toString());
+        
+        reportBean.setReportURLName("userid=irgc/irgc@orcl&domain=classicdomain&report=C:/IRGC_Reports/Supplier_Setup_Payable_Detail&");
+        reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESTYPE,
+                                        "CACHE"); // which will be one of the [cashe - file - mail - printer]
+        reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESFORMAT,
+                                        "PDF"); // Which will be onr of the [HTML - HTML CSS - PDF - SPREADSHEET- RTF].
+        reportBean.setReportParameter("paramform", "no");
+
+        url = reportBean.getReportServerURL();
+        System.out.println("Url => " + url);
+        reportBean.openUrlInNewWindow(url);
+    }
+
+    public void getBankSetupReport(ActionEvent actionEvent) {
+        // Add event code here...
+        String url = "";
+        reportBean.setReportURLName("userid=irgc/irgc@orcl&domain=classicdomain&report=C:/IRGC_Reports/Bank_Setup_BnkCsh_Detail&");
+        reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESTYPE,
+                                        "CACHE"); // which will be one of the [cashe - file - mail - printer]
+        reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESFORMAT,
+                                        "PDF"); // Which will be onr of the [HTML - HTML CSS - PDF - SPREADSHEET- RTF].
+        reportBean.setReportParameter("paramform", "no");
+
+        url = reportBean.getReportServerURL();
+        System.out.println("Url => " + url);
+        reportBean.openUrlInNewWindow(url);
+        
+    }
+
+    public void getCashSetupReport(ActionEvent actionEvent) {
+        // Add event code here...
+        String url = "";
+        reportBean.setReportURLName("userid=irgc/irgc@orcl&domain=classicdomain&report=C:/IRGC_Reports/Cash_Setup_BnkCsh_Detail&");
+        reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESTYPE,
+                                        "CACHE"); // which will be one of the [cashe - file - mail - printer]
+        reportBean.setReportServerParam(OracleReportBean.RS_PARAM_DESFORMAT,
+                                        "PDF"); // Which will be onr of the [HTML - HTML CSS - PDF - SPREADSHEET- RTF].
+        reportBean.setReportParameter("paramform", "no");
+
+        url = reportBean.getReportServerURL();
+        System.out.println("Url => " + url);
+        reportBean.openUrlInNewWindow(url);
+    }
 }
